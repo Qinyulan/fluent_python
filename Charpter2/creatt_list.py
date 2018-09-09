0eeecbe9acaf84380a4faa901086890a0d58ceef
QIN <manager@163.com>
2018-09-10 02:52:51 +0800

    charpter2

--- Charpter2/creatt_list.py
+++ Charpter2/creatt_list.py
@@ -1,5 +1,7 @@
 symbols = "$%^&*"
-codes = []
-for symbol in symbols:
-    codes.append(ord(symbol))
+# codes = []
+# for symbol in symbols:
+#     codes.append(ord(symbol))
+# print(codes)
+codes = [ord(symbol)for symbol in symbols]
 print(codes)
